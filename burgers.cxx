bdc3ff38e94bb6e7403c856d7d8fbc1c5fb759b0
Michael Stumpf <mstumpf@pc03.pool.tp1.uni-duesseldorf.de>
2016-01-13 17:48:57 +0100

    lab10 done

--- burgers.cxx
+++ burgers.cxx
@@ -7,16 +7,15 @@
 using namespace std;
 //---------------------------------------
 void writeToFile(const double* const u, const string s, const double dx,
-                 const double xmin, const int N);
+                 const double xmin, const int N, const double t);
 void initialize(double* const u1, double* const u0, const double dx,const double dt, const double xmin,
                 const int N);
 void step(double* const u2, const double* const u1,const double* const u0,
           const double dt, const double dx, const int N);
-
 //---------------------------------------
 int main(){
 
-  const double tEnd = 0.15 ;
+  const double tEnd = 0.20 ;
 
 
   const int N  = 64;
@@ -28,15 +27,15 @@ int main(){
   const int Na = 10;
   const int Nk = int(tEnd/Na/dt);
 
-  double* u0 = new double[N];
-  double* u1 = new double[N];
-  double* u2 = new double[N];
+  double* u0 = new double[N];	//u von n-1
+  double* u1 = new double[N];	//u von n
+  double* u2 = new double[N];	//u von n+1
   double* h;
   stringstream strm;
 
   initialize(u1,u0,dx,dt, xmin,N);
 
-  writeToFile(u0, "u_0", dx, xmin, N);
+  writeToFile(u1, "u_0", dx, xmin, N, t);
 
   cout << "Nk = " << Nk << endl;
 
@@ -44,13 +43,16 @@ int main(){
   {
    for(int j=0; j<Nk; j++){
 
-      // step + swap here
-
+      step(u2, u1, u0, dt, dx, N);
+      h  = u0;
+      u0 = u1;
+      u1 = u2;
+      u2 =  h;
       t +=dt;
    }
    strm.str("");
    strm << "u_" << i;
-   writeToFile(u0, strm.str(), dx, xmin, N);
+   writeToFile(u1, strm.str(), dx, xmin, N, t);
   }
 
   cout << "t = " << t << endl;
@@ -64,8 +66,11 @@ int main(){
 void step(double* const u2, const double* const u1,const double* const u0,
           const double dt, const double dx, const int N)
 {
-
-
+    u2[0]= u0[0] - dt/dx * u1[0] * (u1[1]-u1[N-1]);
+  for(int i=1; i<N-1; i++){
+    u2[i]= u0[i] - dt/dx * u1[i] * (u1[i+1]-u1[i-1]);
+  }
+    u2[N-1]= u0[N-1] - dt/dx * u1[N-1] * (u1[0]-u1[N-2]);
 }
 //-----------------------------------------------
 void initialize(double* const u1, double* const u0, const double dx,
@@ -75,18 +80,21 @@ void initialize(double* const u1, double* const u0, const double dx,
    for(int i=0; i<N; i++)
    {
      double x = xmin + i*dx;
-
-     
+     u   = sin(2*M_PI*x);
+     ux  = 2*M_PI*cos(2*M_PI*x);
+     uxx = -4*pow(M_PI,2)*sin(2*M_PI*x);
+     u1[i] = u;
+     u0[i] = u - dt* (-u)*ux + 1/2*dt*dt * (2*u*pow(ux,2) + pow(u,2) * uxx);
    }
 }
 //-----------------------------------------------
 void writeToFile(const double* const u, const string s, const double dx,
-                 const double xmin, const int N)
+                 const double xmin, const int N, const double t)
 {
    ofstream out(s.c_str());
    for(int i=0; i<N; i++){
      double x = xmin + i * dx;
-     out << x << "\t" << u[i] << endl;
+     out << x << "\t" << u[i] << "\t" << x + sin(2*M_PI*x)*t << "\t" << sin(2*M_PI*x) << endl;
    }
    out.close();
 }
